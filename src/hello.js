070937f32a4a34377f3a0db69b6ee8cd3009c115
Amey  Pankanti <pankantiameya@gmail.com>
2023-04-25 23:21:02 +0530

    Intro to react

--- src/hello.js
+++ src/hello.js
@@ -1,4 +1,10 @@
+import "./hello.css";
 function Hello(){
-    return <h1>This is component</h1>;
+    return (
+    <div class="hello">
+    <h1>This is component</h1>
+    <h2>Say hello to React</h2>
+    </div>
+    );
 }
 export default Hello;
